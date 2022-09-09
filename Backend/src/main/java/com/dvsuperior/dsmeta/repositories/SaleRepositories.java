7e1aa02c6bda51c3976a1bba458e9e4cf942b016
Ragazzi147 <91346278+Ragazzi147@users.noreply.github.com>
2022-09-09 17:07:27 -0300

    H2

---
+++ Backend/src/main/java/com/dvsuperior/dsmeta/repositories/SaleRepositories.java
@@ -0,0 +1,9 @@
+package com.dvsuperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dvsuperior.dsmeta.entities.Sale;
+
+public interface SaleRepositories extends JpaRepository <Sale, Long> {
+
+}
